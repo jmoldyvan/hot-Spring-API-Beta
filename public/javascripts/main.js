980071ef9349425cdcad53a31e6e81a2508d03c8
jordan Moldovan <jmoldyvan@gmail.com>
2022-07-06 22:35:03 -0700

    update 7/6/22

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,3 @@
-
-
 function getLongAndLat() {
   return new Promise((resolve, reject) =>
       navigator.geolocation.getCurrentPosition(resolve, reject)
@@ -15,24 +13,118 @@ async function currLatLng() {
   console.log(typeof latLng.lat);
   return latLng
 }
-currLatLng()
-// async function sitesWithinRad(){
-//   let userLatLng = await currLatLng()
-//   console.log(userLatLng);
-//   let response = await fetch('/nearest', { 
-//     method: 'POST', 
-//     headers: { 'Content-Type': 'application/json' }, 
-//   body: JSON.stringify(userLatLng), 
-//   })
-//   let data = await response.json()
-//   console.log(data);
-//   console.log(results);
 
+async function getFive(){
+  let latLng = await currLatLng()
+  let response = await fetch ('/api/nearest', {
+    method: 'post', body: JSON.stringify(latLng),
+    headers: { 'Content-Type': 'application/json' }
+  })
+  let data = await response.json()
+  data = [data[0], data[1], data[2], data[3], data[4]]
+  console.log(data);
+  return data
+}
+getFive()
+
+.then(function(data){
+  console.log(data);
+  let firstSelectorName = document.querySelector("#firstName")
+  firstData = data[0]
+  firstSpaceName = firstData.name.replace(/\s+/g,'')
+  firstSelectorName.innerText = `${firstData.name}`
+    let firstSelectorDesc = document.querySelector("#firstDesc")
+    firstSelectorDesc.innerText = `${firstData.description}`
+      let firstSelectorLink = document.querySelector("#firstLink")
+      firstSelectorLink.setAttribute(`href`, `/${firstSpaceName}`)
+
+  let secondSelectorName = document.querySelector("#secondName")
+  secondData = data[1]
+  secondSpaceName = secondData.name.replace(/\s+/g,'')
+  secondSelectorName.innerText = `${secondData.name}`
+    let secondSelectorDesc = document.querySelector("#secondDesc")
+    secondSelectorDesc.innerText = `${secondData.description}`
+      let secondSelectorLink = document.querySelector("#secondLink")
+      secondSelectorLink.setAttribute(`href`, `/${secondSpaceName}`)
+
+  let thirdSelectorName = document.querySelector("#thirdName")
+  thirdData = data[2]
+  thirdSpaceName = thirdData.name.replace(/\s+/g,'')
+  thirdSelectorName.innerText = `${thirdData.name}`
+    let thirdSelectorDesc = document.querySelector("#thirdDesc")
+    thirdSelectorDesc.innerText = `${thirdData.description}`
+      let thirdSelectorLink = document.querySelector("#thirdLink")
+      thirdSelectorLink.setAttribute(`href`, `/${thirdSpaceName}`)
+
+  let fourthSelectorName = document.querySelector("#fourthName")
+  fourthData = data[3]
+  fourthSpaceName = fourthData.name.replace(/\s+/g,'')
+  fourthSelectorName.innerText = `${fourthData.name}`
+    let fourthSelectorDesc = document.querySelector("#fourthDesc")
+    fourthSelectorDesc.innerText = `${fourthData.description}`
+      let fourthSelectorLink = document.querySelector("#fourthLink")
+      fourthSelectorLink.setAttribute(`href`, `/${fourthSpaceName}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// https://codepen.io/jamesqquick/pen/XWJxBQv?editors=1010
+// https://codepen.io/ChrisBarberRiley/pen/eYdBXPP
+// I could hardocde an object of names:nospace names
+// 
+// let searchable = [
+//   'Elastic',
+//   'PHP',
+//   'Something about CSS',
+//   'How to code',
+//   'JavaScript',
+//   'Coding',
+//   'Some other item',
+// ];
+
+
+// then do the below
+
+// const searchInput = document.getElementById('search');
+// const searchWrapper = document.querySelector('.wrapper');
+// const resultsWrapper = document.querySelector('.results');
+
+// searchInput.addEventListener('keyup', () => {
+//   let results = [];
+//   let input = searchInput.value;
+//   if (input.length) {
+//     results = searchable.filter((item) => {
+//       return item.toLowerCase().includes(input.toLowerCase());
+//     });
+//   }
+//   renderResults(results);
+// });
+
+// function renderResults(results) {
+//   if (!results.length) {
+//     return searchWrapper.classList.remove('show');
+//   }
+
+//   const content = results
+//     .map((item) => {
+//       return `<li>${item}</li>`;
+//     })
+//     .join('');
+
+//   searchWrapper.classList.add('show');
+//   resultsWrapper.innerHTML = `<ul>${content}</ul>`;
 // }
 
-.then(latLng => fetch('/api/nearest', { method: 'post', body: JSON.stringify(latLng), 
-headers: { 'Content-Type': 'application/json' } }))
-.then(response => response.json())
-.then(results => console.log(results))
 
-// sitesWithinRad()
+// then add event listener on the click of <ul>content</ul> to populate the url
--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const cors = require('cors')
 const PORT = 8000
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
-// var path = require('path')
+const path=require("path");
+const ejs=require("ejs");
 
 app.use(cors())
 
@@ -25,41 +26,84 @@ MongoClient.connect(dbConnectionStr)
 
 // SET MIDDLEWARE
 app.set('view engine','ejs')
-// app.set("views", __dirname + "views")
+// app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-// app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/', (req,res) => {
-    res.render(__dirname + "/views/index.ejs");
-    // db.collection('hot-spring-col').find().toArray()
-    // .then(data=>)
+
+app.get('/', async (req,res) => {
+    function comparator(a, b) {
+        if (a[0] < b[0]) return -1;
+        if (a[0] > b[0]) return 1;
+        return 0;
+      }
+    let data =  await db.collection('hot-spring-col').find().toArray()
+    
+       let arrList = data.map((x)=> `${x.name}--${x.description}--${x.area}--${x.state}`)
+       arrList = arrList.map((x) => x.split('--'))
+       arrList = arrList.sort((comparator))
+       let nameList = data.map((x)=> x.name)
+       let descList = data.map((x)=> x.description)
+       let areaList = data.map((x)=> x.area)
+       let stateList = data.map((x)=> x.state)
+    //    console.log(arrList);
+       res.render('index', {allInfo: arrList, nameInfo: nameList, descInfo: descList, areaInfo: areaList, stateInfo: stateList})
 })
 
-app.get('/:link', (req,res) => {
-    const newPageLink = req.params.link.toLowerCase()
-    console.log(newPageLink);
-    res.render(__dirname + `/views/${newPageLink}.ejs`);
+app.get('/map/data', async (req,res) =>{
+    console.log('map req started');
+    let allApiData = await db.collection('hot-spring-col').find().toArray()
+    res.send(allApiData)
+
 })
 
-app.post('/api/nearest', (req, res) => {
+app.get('/:link', async (req,res) => {
+    let fileList = ['about', 'contact', 'services', 
+    'blogsinglepost', 'blogpage2', 'portofolio3', 'map']
+
+    const newPageLink = req.params.link
+    // console.log(newPageLink);
+    if(fileList.includes(newPageLink)){
+    return res.render(__dirname + `/views/${newPageLink}.ejs`);
+    }
+
+    let data =  await db.collection('hot-spring-col').find().toArray()
+        let currSiteLink = data.filter((x) => x.nospacename == newPageLink)
+        currSiteLink = currSiteLink[0]
+        // console.log(currSiteLink);
+        res.render('portofoliodetail', {siteObj: currSiteLink});
+})
+
+app.post('/api/nearest', async (req, res) => {
     console.log('POST GOT');
     const {  lng, lat } = req.body
-    console.log({lng, lat});
-    db.collection('hot-spring-col').find({
-      loc: {
-         $geoWithin: { $centerSphere: [ [ lng, lat ], 50000/3963.2 ] }
-             }
+    // console.log({lng, lat}); ''
+    results = await db.collection('hot-spring-col').find({
+    loc: {
+        $near:{
+        $geometry: {type: "Point", coordinates: [ lng, lat ],},
+        // 500mi = 804672meter  150mi = 241402meter 50mi = 80467.2 10mi=1609.34 5=8046.72
+        $maxDistance: 804672}
+    }
     }).toArray()
-    .then(results => {
-        console.log(results)
-        res.send(results)
-    })
+    res.send(results)
   })
 
 
-
+  
+    // ******************************************************
+    // **if $near doesnot work**
+    // config.set("displayBatchSize",300)
+    // db.collection('hot-spring-col').find({
+    //   loc: {
+    //      $geoWithin: { $centerSphere: [ [ lng, lat ], 100/3963.2 ] }
+    //          }
+    // }).toArray()
+    // .then(results => {
+    //     console.log(results)
+    //     res.send(results)
+    // })
 
 
 
